5a834d4d17312a459446b03974b0678ec8defb7d
Taylor Le <taylorlschoolmail@gmail.com>
2018-06-11 15:22:47 +0000

    create awesome platformer game

---
+++ projects/platformer/js/init/cannon.js
@@ -0,0 +1,34 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.collectable = window.opspark.collectable || {};
+    let cannon = window.opspark.cannon;
+    
+    /**
+     * init: Initialize all cannons.
+     * 
+     * Add as many cannons as necessary (at least 3) to make your level challenging. 
+     *
+     * The following functions are available to you:
+     *  cannon.create.onTop(xLocation);
+     *  cannon.create.onBottom(xLocation);
+     *  cannon.create.onLeft(yLocation);
+     *  cannon.create.onRight(yLocation);
+     */ 
+    cannon.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        // example: 
+        cannon.create.onTop(250);
+        cannon.create.onTop(500);
+        cannon.create.onLeft(500);
+        cannon.create.onBottom(600);
+        
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,48 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the platform.create() method to create platforms for the level. 
+     * 
+     * platform.create() takes these arguments:
+     *      
+     *      platform.create(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    platform.init = function (game) {
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        platform.create(0, game.world.height - 32, 12, 8);
+
+        // example:
+        platform.create(240, 555);
+        platform.create(360, 444);
+        platform.create(420, 333);
+        platform.create(580, 222);
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    };
+})(window);
